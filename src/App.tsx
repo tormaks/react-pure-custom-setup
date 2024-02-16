d29fed080f12e42b98a45c3d7f463e2c4f509e21
tormaks <roadtopatience@gmail.com>
2024-02-16 11:41:04 +0300

    feat: add themes and style organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React, { Suspense } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import './index.scss';
+import { Link, Route, Routes } from 'react-router-dom';
+import './styles/index.scss';
 import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy';
 import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>
+        TOGGLE
+      </button>
       <Link to={'/about'}>About</Link>
       <Link to={'/'}>Main</Link>
       <Suspense fallback={<div>...</div>}>

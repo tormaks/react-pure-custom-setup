29ad9c7793a32a9b56120807b1c4e43cfc2c462b
tormaks <roadtopatience@gmail.com>
2024-02-16 11:03:56 +0300

    feat: add router and lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import './index.scss';
+import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy';
+import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
 
 const App = () => {
   return (
     <div className="app">
-      <h1></h1>
+      <Link to={'/about'}>About</Link>
+      <Link to={'/'}>Main</Link>
+      <Suspense fallback={<div>...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageLazy />}/>
+          <Route path={'/'} element={<MainPageLazy />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
